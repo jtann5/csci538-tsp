293ce13b5dc1f817728ce31548b71a63201236e5
Jacob Tanner <35159340+jtann5@users.noreply.github.com>
2025-04-18 15:58:56 -0600

    Basic format

--- README.md
+++ README.md
@@ -3,7 +3,15 @@
 
  ## Graph Format
 ```
-
+{
+    "nodes": [0, 1, 2, 3],
+    "edges": [
+        {"from": 0, "to": 1, "weight": 10},
+        {"from": 0, "to": 2, "weight": 15},
+        {"from": 1, "to": 2, "weight": 20},
+        {"from": 2, "to": 3, "weight": 25}
+    ]
+}
 ```
 
 ## Explantation of Files
--- llm_solver.py
+++ llm_solver.py
@@ -1,1 +1,57 @@
-import graph
+from openai import OpenAI
+import time
+
+# Make sure to export environment variable OPENAI_API_KEY
+
+TSP_4o_ID = 'asst_sv7Y0koVte4odmHCmnJoKz8V'
+TSP_o1_ID = 'asst_QrJ43lrH8FWXD8xc7Y3l8ayB'
+
+class LLMSolver():
+  def __init__(self, model="gpt-4o"):
+    if model == "gpt-4o":
+      self.ASSISTANT_ID = TSP_4o_ID
+    elif model == "o1":
+      self.ASSISTANT_ID = TSP_o1_ID
+
+    self.client = OpenAI()
+    self.thread = self.client.beta.threads.create()
+
+  def set_assistant(self, model):
+    if model == "gpt-4o":
+      self.ASSISTANT_ID = TSP_4o_ID
+    elif model == "o1":
+      self.ASSISTANT_ID = TSP_o1_ID
+
+  def analyze(self, text):
+    # Add message to a thread
+    message = self.client.beta.threads.messages.create(
+        thread_id=self.thread.id,
+        role="user",
+        content=text,
+    )
+
+    # Create a run
+    run = self.client.beta.threads.runs.create(
+      thread_id=self.thread.id,
+      assistant_id=self.ASSISTANT_ID,
+    )
+      
+    # Check for status of run
+    while run.status in ['queued', 'in_progress', 'cancelling']:
+      time.sleep(1) # Wait for 1 second
+      run = self.client.beta.threads.runs.retrieve(
+        thread_id=self.thread.id,
+        run_id=run.id
+      )
+
+    # return message
+    if run.status == 'completed': 
+      message_response = self.client.beta.threads.messages.list(
+        thread_id=self.thread.id
+      )
+      messages = message_response.data
+      latest_message = messages[0]
+
+      return latest_message.content[0].text.value
+    else:
+      return run.status
